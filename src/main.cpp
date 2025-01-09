71a5d280c281c6f93e9f9501c33f136a1eb6f9ef
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-09 08:18:19 +0700

    feat<>: testing motor dc connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,91 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define ENA 9
+#define IN1 7
+#define IN2 8
+
+#define ENB 10
+#define IN3 4
+#define IN4 5
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  pinMode(ENB, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 }
+
+void maju() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 150);
+
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 150);
+}
+
+void mundur() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  analogWrite(ENA, 150);
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENB, 150);
+}
+
+void stop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 0);
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 0);
+}
+
+void putarKanan() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 150);
+
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 150);
+}
+
+void putarKiri() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 150);
+
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 150);
+}
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Serial.println("Maju");
+  maju();
+  delay(2000);
+
+  Serial.println("Mundur");
+  mundur();
+  delay(2000);
+
+  Serial.println("Putar Kanan");
+  putarKanan();
+  delay(2000);
+
+  Serial.println("Putar Kiri");
+  putarKiri();
+  delay(2000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
