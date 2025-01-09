7c87aed4e01e51d27e960c7e461091fb2d465de7
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-09 21:58:00 +0700

    feat<>: create transmitter code for hand controller

---
+++ src/transmitter.cpp
@@ -0,0 +1,53 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_MPU6050.h>
+
+#define CE_PIN 9
+#define CSN_PIN 10
+
+RF24 radio(CE_PIN, CSN_PIN);
+const byte address[6] = "00001";
+Adafruit_MPU6050 mpu;
+
+void setup() {
+   Serial.begin(9600);
+
+   radio.begin();
+   radio.openWritingPipe(address);
+   radio.setPALevel(RF24_PA_LOW);
+   radio.stopListening();
+
+   if(!mpu.begin()) {
+      Serial.println("MPU6050 is undetected! ");
+      while(1);
+   }
+
+   Serial.println("MPU6050 succeded initialized! ");
+}
+
+void loop() {
+   sensors_event_t a, g, temp;
+   mpu.getEvent(&a, &g, &temp);
+
+   float data[3] = {
+      a.acceleration.x,
+      a.acceleration.y,
+      a.acceleration.z,
+   }
+
+   bool success = radio.write(&data, sizeof(data));
+
+   if(success) {
+      Serial.print("Data sent: ");
+      Serial.print(data[0]); Serial.print(", ");
+      Serial.print(data[1]); Serial.print(", ");
+      Serial.println(data[2]);
+   } else {
+      Serial.println("Data failed to send! ");
+   }
+
+   delay(100);
+}
