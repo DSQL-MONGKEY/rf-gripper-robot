d8841dc1a34df6df5f3e83bf5bbfd5509c4a4b03
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-12 22:21:36 +0700

    chore<>: change serial print

--- src/nrf-connection-testing.cpp
+++ src/nrf-connection-testing.cpp
@@ -7,16 +7,16 @@
 RF24 radio(9, 10); // CE=9, CSN=10
 
 void setup() {
-    Serial.begin(9600);
-    Serial.println("Memulai tes koneksi NRF24L01...");
+   Serial.begin(9600);
+   Serial.println("Memulai tes koneksi NRF24L01...");
 
-    if (!radio.begin()) {
-        Serial.println("NRF24L01 tidak terdeteksi!");
-        while (1); // Berhenti di sini jika modul tidak terdeteksi
-    }
+   if (!radio.begin()) {
+      Serial.println("NRF24L01 tidak terdeteksi!");
+      while (1); // Berhenti di sini jika modul tidak terdeteksi
+   }
 
-    Serial.println("NRF24L01 terdeteksi!");
-    radio.printDetails(); // Menampilkan detail konfigurasi modul
+   Serial.println("NRF24L01 terdeteksi!");
+   radio.printDetails(); // Menampilkan detail konfigurasi modul
 }
 
 void loop() {}
