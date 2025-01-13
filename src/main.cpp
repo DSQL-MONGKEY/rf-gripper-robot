32f3c7a84af33dca9c7cf84ddc8bded9d7768312
Dimpfe <dimasprasetyo7696@gmail.com>
2025-01-13 23:30:39 +0700

    feat<>: change loop function

--- src/main.cpp
+++ src/main.cpp
@@ -21,42 +21,47 @@ RF24 radio(CE_PIN, CSN_PIN);
 const byte address[6] = "00001";
 
 void setup() {
-  Serial.begin(9600);
-  // Serial.println("Checking SPI device...");
-  // byte response = SPI.transfer(0xFF);
-
-  // Serial.print("Response SPI: ");
-  // Serial.println(response, HEX);
-
-  radio.begin();
+   Serial.begin(9600);
   
-  if(!radio.begin()) {
-    Serial.println("======= NRF24L01 is unconnected =======");
-  } else {
-    Serial.println("======= NRF24L01 is connected =======");
-    radio.openReadingPipe(0, address);
-    radio.setPALevel(RF24_PA_LOW);
-    radio.startListening();
-  }
 
-  pinMode(ENA, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
+   radio.begin();
+   
+   if(!radio.begin()) {
+      Serial.println("======= NRF24L01 is unconnected =======");
+      Serial.println("[ERROR] NRF24L01 not detected. Please check the wiring!");
+      while(1);
+   } else {
+      Serial.println("======= NRF24L01 is connected =======");
+      Serial.println("[SUCCESS] NRF24L01 detected and initialized.");
+      radio.openReadingPipe(0, address);
+      radio.setPALevel(RF24_PA_LOW);
+      
+      Serial.println("Checking SPI device...");
+      byte response = SPI.transfer(0xFF);
+      Serial.print("Response SPI: ");
+      Serial.println(response, HEX);
+      
+      radio.startListening();
+   } 
 
-  pinMode(ENB, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
+   pinMode(ENA, OUTPUT);
+   pinMode(IN1, OUTPUT);
+   pinMode(IN2, OUTPUT);
+
+   pinMode(ENB, OUTPUT);
+   pinMode(IN3, OUTPUT);
+   pinMode(IN4, OUTPUT);
 }
 
 void maju() {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  analogWrite(ENA, 150);
+   digitalWrite(IN1, HIGH);
+   digitalWrite(IN2, LOW);
+   analogWrite(ENA, 150);
 
 
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-  analogWrite(ENB, 150);
+   digitalWrite(IN3, HIGH);
+   digitalWrite(IN4, LOW);
+   analogWrite(ENB, 150);
 }
 
 
@@ -102,27 +107,28 @@ void putarKiri() {
 
 
 void loop() {
-    if(radio.available()) {
+   if(radio.available()) {
       float data[3];
       radio.read(&data, sizeof(data));
 
-      Serial.println("Data diterima: ");
+      Serial.print("Data diterima: ");
       Serial.print(data[0]); Serial.print(", ");
       Serial.print(data[1]); Serial.print(", ");
-      Serial.print(data[2]);
+      Serial.println(data[2]);
+      Serial.println("----------------------------------------");
 
       if(data[1] > 1.0) {
-        maju();
+         maju();
       } else if(data[1] < -1.0) {
-        mundur();
+         mundur();
       } else if(data[0] > 1.0) {
-        putarKanan();
+         putarKanan();
       } else if(data[0] < -1.0) {
-        putarKiri();
+         putarKiri();
       } else {
-        stop();
+         stop();
       };
-    };
+   };
 }
 
 
